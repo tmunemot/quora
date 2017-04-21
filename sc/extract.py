c833a22667ba34d2022a3de18d9130694fb0a84c
Taihei Munemoto <taihei@Taiheis-MacBook-Pro-3.local>
2017-04-21 04:38:14 -0400

    intermediate word2vec feature implementation

--- sc/extract.py
+++ sc/extract.py
@@ -11,6 +11,12 @@ import multiprocessing
 import warnings
 import tqdm
 import preprocess
+import scipy.stats
+import scipy.spatial
+import gensim
+
+model_w2v = None
+path_w2v = "./model/GoogleNews-vectors-negative300.bin"
 
 def parse_args(argv):
 	parser = argparse.ArgumentParser(description="Performs feature extraction.")
@@ -28,6 +34,9 @@ def worker(args):
 	
 	# base features
 	extract_base(q1, q2, row)
+    
+    # word2vec
+    extract_w2v(q1, q2, row)
 
 	row["classlabel"] = args["classlabel"]
 	
@@ -39,13 +48,48 @@ def extract_base(q1, q2, row):
 	s2 = set(q2)
 	row["base_wlen_common"] = len(s1 & s2)
 	row["base_wlen_xor"] = len(s1 ^ s2)
-	
+
+def basic_stats(x, row, prefix):
+    row[prefix + "_mean"] = np.mean(x)
+    row[prefix + "_std"] = np.std(x, ddof=1)
+    row[prefix + "_skew"] = scipy.stats.skew(x)
+    row[prefix + "_kurtosis"] = scipy.stats.kurtosis(x)
+    row[prefix + "_min"] = np.min(x)
+    row[prefix + "_max"] = np.max(x)
+    row[prefix + "_range"] = row[prefix + "_max"] - row[prefix + "_min"]
+    row[prefix + "_q1"] = np.percentile(x, 25)
+    row[prefix + "_median"] = np.median(x)
+    row[prefix + "_q3"] = np.percentile(x, 75)
+    row[prefix + "_iqrange"] = row[prefix + "_q3"] - row[prefix + "_q1"]
+    row[prefix + "_mad"] = np.abs(x - row[prefix + "_mean"]) / float(len(x))
+    row[prefix + "_coefvar"] = scipy.stats.variation(x)
+
+def extract_w2v(q1, q2):
+    global model_w2v
+    if model_w2v is None:
+        model_w2v = gensim.models.Word2Vec.load_word2vec_format(path_w2v, binary=True)
+    #dist = scipy.spatial.distance.cdist(q1_glove, q2_glove, 'cosine')
+    #dist = dist[np.triu_indices(dist.shape[0])]
+    s = []
+    for w1 in q1:
+        for w2 in q2:
+            s.append(model_w2v.wv.similarity(w1, w2))
+    basic_stats(s, row, "w2v_")
+
+
 def extract(csvfile, outfile, n_threads):
 	df = pd.read_csv(csvfile)
 	print '-' * 30 
-	print 'preprocess ' + csvfile	
+	print 'preprocess ' + csvfile
 	df = preprocess.preprocess_texts(df)
-	print '-' * 30	
+	print '-' * 30
+    print '-' * 30
+    print 'load glove weights '
+    filepath = os.path.join(preprocess.GLOVE_DIR, "")
+    global glove_weights
+    tk = fit_tokenizer(df)
+    glove_weights, nw = preprocess.load_weights(filepath, tk)
+    print '-' * 30
 	worker_args = [{
 		"q1": r["question1"],
 		"q2": r["question2"],
