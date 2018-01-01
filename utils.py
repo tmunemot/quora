556946eb8aec2db240309993247cbab910d6e11e
Taihei Munemoto <tmunemot@gmail.com>
2018-01-01 16:28:03 -0500

    Move commonly used functions to utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,51 @@
 import sys
 import os
 import time
+import errno
 import multiprocessing
+import sklearn.metrics
+import numpy as np
+import pandas as pd
+
+
+def mkdir_p(path):
+    """
+        emulate to "mkdir -p" in Python
+        https://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+        
+        Args:
+        path: path to a new directry
+        
+        Returns:
+        None
+        """
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def add_evaluate_base_args(parser):
+    parser.add_argument("train", help="training data")
+    parser.add_argument("val", help="validation data")
+    parser.add_argument("dev", help="development data used during training")
+    parser.add_argument("outdir", help="output directory")
+
 
 def notify(section, dlen=75):
+    """
+        an utility function for reporting processing time
+        
+        Args:
+        section: a section id printed along with a processing time
+        dlen: length of a divider
+        
+        Returns:
+        None
+    """
     if notify.t_start != -1:
         # print time elapsed
         t = time.time()
@@ -21,8 +63,49 @@ def notify(section, dlen=75):
 notify.t_start = -1
 notify.section_id = 1
 
-def main(argv):
-    print "do not run"
 
-if __name__ == '__main__':
-    exit(main(sys.argv[1:]))
+def create_prediction_df(y, y_proba, ids):
+    """
+        create a dataframe that contians predictions
+        
+        Args:
+        y: target labels
+        y_score: predicted probabilities ranging 0 to 1
+        
+        Returns:
+        a dataframe contains predicted probabilities
+    """
+    df_id = pd.DataFrame(ids, columns=["id"])
+    df_proba = pd.DataFrame(y_proba, columns=["proba"])
+    df_cl = pd.DataFrame(y, columns=["is_duplicate"])
+    return pd.concat([df_id, df_proba, df_cl], axis=1)
+
+
+def metrics(y, y_score):
+    """
+        calculate evaluation metrics given predictions
+        
+        Args:
+        y: target labels
+        y_score: predicted probabilities ranging 0 to 1
+        
+        Returns:
+        a trained xgboost model object
+    """
+    y_pred = (y_score > 0.5).astype(int)
+    
+    values = OrderedDict()
+    
+    # accuracy, precision, recall, f-measure, auc, etc.
+    values["accuracy"] = sklearn.metrics.accuracy_score(y, y_pred)
+    values["precision"] = sklearn.metrics.precision_score(y, y_pred)
+    values["recall"] = sklearn.metrics.recall_score(y, y_pred)
+    values["f1"] = sklearn.metrics.f1_score(y, y_pred)
+    values["auc"] = sklearn.metrics.roc_auc_score(y, y_score)
+    values["average_precision"] = sklearn.metrics.average_precision_score(y, y_score)
+    values["brier_score_loss"] = sklearn.metrics.brier_score_loss(y, y_score)
+    values["log_loss"] = sklearn.metrics.log_loss(y, y_score, eps=_EPSILON)
+    df_metrics = pd.DataFrame.from_dict([values])
+    conf = sklearn.metrics.confusion_matrix(y, y_pred).astype(int)
+    return df_metrics, conf
+
--- xgboost_evaluation.py
+++ xgboost_evaluation.py
@@ -6,9 +6,9 @@ import argparse
 import numpy as np
 import pandas as pd
 import xgboost as xgb
-import errno
+
 from collections import OrderedDict
-import sklearn.metrics
+from utils import add_evaluate_base_args, mkdir_p, create_prediction_df, metrics
 
 
 XGB_PARAMS={
@@ -30,79 +30,6 @@ def parse_args(argv):
     add_evaluate_base_args(parser)
     # TODO: add an option for xgboost hyperparameters
     return parser.parse_args(argv)
-
-
-def add_evaluate_base_args(parser):
-    parser.add_argument("train", help="training data")
-    parser.add_argument("val", help="validation data")
-    parser.add_argument("dev", help="development data used during training")
-    parser.add_argument("outdir", help="output directory")
-
-
-def mkdir_p(path):
-    """
-        emulate to "mkdir -p" in Python
-        https://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
-        
-        Args:
-        path: path to a new directry
-        
-        Returns:
-        None
-    """
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
-def create_prediction_df(y, y_proba, ids):
-    """
-        create a dataframe that contians predictions
-        
-        Args:
-        y: target labels
-        y_score: predicted probabilities ranging 0 to 1
-        
-        Returns:
-        a dataframe contains predicted probabilities
-    """
-    df_id = pd.DataFrame(ids, columns=["id"])
-    df_proba = pd.DataFrame(y_proba, columns=["proba"])
-    df_cl = pd.DataFrame(y, columns=["is_duplicate"])
-    return pd.concat([df_id, df_proba, df_cl], axis=1)
-
-
-def metrics(y, y_score):
-    """
-        calculate evaluation metrics given predictions
-        
-        Args:
-        y: target labels
-        y_score: predicted probabilities ranging 0 to 1
-        
-        Returns:
-        a trained xgboost model object
-    """
-    y_pred = (y_score > 0.5).astype(int)
-
-    values = OrderedDict()
-
-    # accuracy, precision, recall, f-measure, auc, etc.
-    values["accuracy"] = sklearn.metrics.accuracy_score(y, y_pred)
-    values["precision"] = sklearn.metrics.precision_score(y, y_pred)
-    values["recall"] = sklearn.metrics.recall_score(y, y_pred)
-    values["f1"] = sklearn.metrics.f1_score(y, y_pred)
-    values["auc"] = sklearn.metrics.roc_auc_score(y, y_score)
-    values["average_precision"] = sklearn.metrics.average_precision_score(y, y_score)
-    values["brier_score_loss"] = sklearn.metrics.brier_score_loss(y, y_score)
-    values["log_loss"] = sklearn.metrics.log_loss(y, y_score, eps=_EPSILON)
-    df_metrics = pd.DataFrame.from_dict([values])
-    conf = sklearn.metrics.confusion_matrix(y, y_pred).astype(int)
-    return df_metrics, conf	
 
 
 def fit(df, outfile, df_test):
